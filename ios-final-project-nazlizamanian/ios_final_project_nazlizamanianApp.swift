8ab5485ad171cfe4ad1abf37c85fd326a1e48372
Nazli Zamanian <nazlizamanian@gmail.com>
2024-06-19 11:26:55 +0200

    tt

---
+++ ios-final-project-nazlizamanian/ios_final_project_nazlizamanianApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_final_project_nazlizamanianApp.swift
+//  ios-final-project-nazlizamanian
+//
+//  Created by Nazli  on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_final_project_nazlizamanianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
